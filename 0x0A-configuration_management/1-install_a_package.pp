8737084ca3c5a748d5a7f861799237573d965549
Noble <neluwah@gmail.com>
2023-12-26 09:36:18 -0800

    Update: Corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,8 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+package { 'werkzeug':
+  ensure   => '2.0.2',  # Specify a version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
