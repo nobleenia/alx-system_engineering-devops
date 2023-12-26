5c67c156429ce0d4b228bd04c59d486da0afdfd9
Noble <neluwah@gmail.com>
2023-12-26 08:55:14 -0800

    Upate: Corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install flask using puppet
+# install puppet-lint -v 2.5.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
