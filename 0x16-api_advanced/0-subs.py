da5ffb6293fe922c30893656f6ffd513fa232a75
Noble <neluwah@gmail.com>
2024-02-14 00:51:08 -0800

    Update: Corrections 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,11 +11,11 @@ def number_of_subscribers(subreddit):
     Return the total number of subscribers on a given subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
-    response = requests.get(url, headers=headers).json()
-    subscribers = response.get('data', {}).get('subscribers')
-    if not subscribers:
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    return subscribers
+    results = response.json().get("data")
+    return results.get("subscribers")
