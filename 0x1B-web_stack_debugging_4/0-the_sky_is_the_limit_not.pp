fe4da732b59cbfb69dee889e1878344215871aa2
nobleenia <neluwah@gmail.com>
2024-02-22 20:29:06 +0000

    Update: Corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
 # Puppet Manifest to Adjust Nginx ULIMIT and Restart Nginx Service
 # The first exec resource is tasked with modifying the Nginx configuration
-# to adjust the ULIMIT setting, which controls the maximum number of open files.
+# to adjust the ULIMIT setting, which controls the maximum open files.
 # Puppet file to replace a line...
 
 exec {'fix--for-nginx':
      command  =>  '/bin/sed -i "s/15/4096/" /etc/default/nginx',
      path     =>  '/usr/local/bin/:/bin/'
-     # provider =>   'shell'
 }
 
 # The second exec resource is responsible for restarting the Nginx service.
@@ -16,5 +15,4 @@ exec {'fix--for-nginx':
 exec {'nginx-restart':
      command =>  '/etc/init.d/nginx restart',
      path    =>  '/etc/init.d'
-     # require =>  Exec['fix--for-nginx']
 }
